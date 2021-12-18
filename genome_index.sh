fc13e02e8dbdfd4f51216d1be60854f3b0f0e9c9
Asma Mechakra <amechakra@binfservms01.unibe.ch>
2021-12-18 22:04:29 +0100

    Maping

--- genome_index.sh
+++ genome_index.sh
@@ -10,7 +10,7 @@
 module add UHTS/Aligner/hisat/2.2.1;
 hisat2-build /data/courses/rnaseq/toxoplasma_de/genome/Mus_musculus.GRCm39.dna.primary_assembly.fa mg_indexed
 
-#run in shell : sbatch genome_index
+#run in shell : sbatch genome_index ....
 
 
 
